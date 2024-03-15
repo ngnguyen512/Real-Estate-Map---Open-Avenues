fb68da7fa3ef89afb9a51d921c6edf58752cff71
ngnguyen512 <133614681+ngnguyen512@users.noreply.github.com>
2024-03-15 16:34:38 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+        {
+           protocol: "http",
+           hostname: "upload.wikimedia.org",
+         },
+        ],
+     },
+};
 
-module.exports = nextConfig;
+
+export default nextConfig;
